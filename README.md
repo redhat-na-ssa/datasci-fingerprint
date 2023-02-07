e8ce6e2dfae714caf9a46547e313152f654b153f
Cory Latschkowski <koree@redhat.com>
2023-02-07 17:50:05 -0600

    add: notebooks and stuff

---
+++ README.md
@@ -0,0 +1,18 @@
+# Fingerprint Prediction Notebooks
+
+<!-- ABOUT THE PROJECT -->
+## The Project
+
+A fingerprint dataset used to explore the various data science tools on the Red Hat OpenShift platform.
+
+### Prerequisites
+
+- [x] Python 3.6+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Acknowledgements
+
+- [Kaggle - Sokoto Coventry Fingerprint Dataset](https://www.kaggle.com/datasets/ruizgara/socofing)
+- [TensorFlow Image Classification](https://www.tensorflow.org/tutorials/images/classification#use_tensorflow_lite)
+- [TensorFlow Lite Image Classification](https://www.tensorflow.org/lite/models/modify/model_maker/image_classification#simple_end-to-end_example)
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#set -e
+
+usage(){
+  # echo "
+  # setup virtualenv:
+  # python3 -m venv venv
+  # "
+
+echo "
+You can run individual functions!
+
+example:
+  setup_demo
+  delete_demo
+"
+}
+
+is_sourced() {
+  if [ -n "$ZSH_VERSION" ]; then 
+      case $ZSH_EVAL_CONTEXT in *:file:*) return 0;; esac
+  else  # Add additional POSIX-compatible shell names here, if needed.
+      case ${0##*/} in dash|-dash|bash|-bash|ksh|-ksh|sh|-sh) return 0;; esac
+  fi
+  return 1  # NOT sourced.
+}
+
+setup_venv(){
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install -q -U pip
+
+  check_venv || usage
+}
+
+check_venv(){
+  # activate python venv
+  [ -d venv ] && . venv/bin/activate || setup_venv
+}
+
+
+setup_dataset(){
+  SCRATCH=scratch
+  DATA_SRC=https://github.com/redhat-na-ssa/demo-rosa-sagemaker-data.git
+  
+  echo "Pulling dataset from ${DATA_SRC} (gross)..."
+
+  git clone "${DATA_SRC}" "${SCRATCH}"/.raw >/dev/null 2>&1 || echo "exists"
+
+  mkdir -p "${SCRATCH}"/{train,models}
+
+  tar -Jxf "${SCRATCH}"/.raw/left.tar.xz -C "${SCRATCH}"/train/ && \
+  tar -Jxf "${SCRATCH}"/.raw/right.tar.xz -C "${SCRATCH}"/train/ && \
+  tar -Jxf "${SCRATCH}"/.raw/real.tar.xz -C "${SCRATCH}" && \
+  tar -Jxf "${SCRATCH}"/.raw/model-v1-full.tar.xz -C "${SCRATCH}"/models
+
+}
+
+setup_demo(){
+  check_venv
+  setup_dataset  
+}
+
+is_sourced && usage || setup_demo
